14fcca9ebe837198ba70f4bdde322c3cc5337033
Pittersss <pedromalaquias608@gmail.com>
2025-02-09 14:45:40 -0300

    changes

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 
 module Main where
-import Fudget
+import Graphics.UI.Gtk
+import Graphics.UI.Gtk.Gdk.Pixbuf
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+
+    initGUI
+
+    window <- windowNew
+    set window [windowTitle := "Exemplo GTK3", windowDefaultWidth := 600, windowDefaultHeight := 600]
+    
+    box <- vBoxNew False 5
+    containerAdd window box
+
+    pixbuf <- pixbufNewFromFile "/home/pedro/Documentos/haskell-project/pokemon-haskell/campo_de_batalha.jpg" -- Substitua pelo caminho real da imagem
+    image <- imageNewFromPixbuf pixbuf
+
+    boxPackStart box image PackNatural 0
+
+    _ <- on window objectDestroy mainQuit
+
+    widgetShowAll window
+
+    mainGUI
+
