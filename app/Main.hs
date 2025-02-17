4ca2550140a00f2a2f21bc5da7b708f676238d41
=Pittersss <pedromalaquias608@gmail.com>
2025-02-17 15:58:02 -0300

    att

--- app/Main.hs
+++ app/Main.hs
@@ -15,28 +15,57 @@ import qualified GI.Gio as Gio
 import qualified GI.Gtk as Gtk
 import Data.GI.Base
 
-escolhaSeuPokemon :: Gtk.Application -> IO ()
-escolhaSeuPokemon app = do
+escolhaSeuPokemon :: Gtk.Application -> Gtk.ApplicationWindow -> IO ()
+escolhaSeuPokemon app w = do
+  Gtk.windowDestroy w
+
+  box <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
+  
   window <- new Gtk.ApplicationWindow [ #application := app,
-                                 #title := "Haskell Gi - Examples - Packed Widgets",
+                                  #title := "Haskell Gi - Examples - Packed Widgets",
+                                  #child := box,
                                   #defaultWidth := 800,
                                   #defaultHeight := 600
                                  ]
+  button1 <- new Gtk.Button [#label := "Pokémon 1",
+                            On #clicked (escolhaSeuPokemon app window)]
+  button2 <- new Gtk.Button [#label := "Pokémon 2",
+                            On #clicked (escolhaSeuPokemon app window)]
+  button3 <- new Gtk.Button [#label := "Pokémon 3",
+                            On #clicked (escolhaSeuPokemon app window)]
+  button4 <- new Gtk.Button [#label := "Pokémon 4",
+                            On #clicked (escolhaSeuPokemon app window)]
+  button5 <- new Gtk.Button [#label := "Pokémon 5",
+                            On #clicked (escolhaSeuPokemon app window)]                                                                                                        
+
+  
+
+  #append box button1
+  #append box button2
+  #append box button3
+  #append box button4
+  #append box button5
+
+  Gtk.widgetSetValign box Gtk.AlignCenter
+
   window.show
 
 activate :: Gtk.Application -> IO ()
 activate app = do
-  -- 
-  button <- new Gtk.Button [#label := "Jogar",
-                            On #clicked (escolhaSeuPokemon app) ]
-  
+  box <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
 
 
   window <- new Gtk.ApplicationWindow [#application := app,
                                        #title := "Pokemon-Haskell",
-                                       #child := button,
+                                       #child := box,
                                        #defaultWidth := 800,
                                        #defaultHeight := 600]
+
+  button <- new Gtk.Button [#label := "Jogar",
+                            On #clicked (escolhaSeuPokemon app window) ]
+  #append box button
+  Gtk.widgetSetValign box Gtk.AlignCenter  -- Alinha horizontalmente no centro
+  Gtk.widgetSetHalign box Gtk.AlignCenter  -- Alinha verticalmente no centro              
   window.show
 
 main :: IO ()
