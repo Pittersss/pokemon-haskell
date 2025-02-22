95044cf5398c8f69c5f0fb25223b874df4df501e
aelsonjrdiniz <aelson.junior.araujo.diniz.cunha@ccc.ufcg.edu.br>
2025-02-22 18:41:09 -0300

    Minha parte

--- app/Main.hs
+++ app/Main.hs
@@ -1,227 +1,184 @@
-{-# LANGUAGE OverloadedStrings, OverloadedLabels, OverloadedRecordDot, ImplicitParams #-}
-module Main where
+import Data.Csv
+import qualified Data.ByteString.Lazy as BL
+import Data.Vector (Vector)
+import Control.Applicative
+import System.Random (randomRIO)
 
+data Pokemon = Pokemon {
+	nome :: String,
+	tipo1 :: String,
+	tipo2 :: String,
+	hp :: Int,
+	fAtk :: Int,
+	fDef :: Int,
+	sAtk :: Int,
+	sDef :: Int,
+	spd :: Int
+} deriving (Show)
 
-{- cabal:
-build-depends: base >= 4.16, haskell-gi-base, gi-gtk4
--}
-import Control.Monad (void)
+data PokemonBattle = PokemonBattle {
+	nomePok :: String,
+	type1 :: String,
+	type2 :: String,	
+	maxHitPoints :: Int,
+	hitPoints :: Int,
+	fAttack :: Int,
+	fDefense :: Int,
+	sAttack :: Int,
+	sDefense :: Int,
+	speed :: Int,
+	level :: Int,
+	atk1 :: String,
+	atk2 :: String,
+	atk3 :: String,
+	atk4 :: String,
+	condition :: String
+}
 
-import Data.Int
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
+data Attack = Attack {
+	name :: String,
+	typing :: String,
+	category :: String,
+	power :: Int,
+	accuracy :: Int,
+	pp :: Int,
+	currentPP :: Int,
+	critical :: Int
+}
 
-import qualified GI.Gio as Gio
-import qualified GI.Gtk as Gtk
-import Data.GI.Base
+data Item = Item {
+	nomeItem :: String,
+	qtde :: Int
+}
 
-<<<<<<< HEAD
+instance FromRecord Pokemon where
+	parseRecord v
+		| length v == 9 = Pokemon <$> v .! 0 <*> v .! 1 <*> v .! 2 <*> v .! 3 <*> v .! 4 <*> v .! 5 <*> v .! 6 <*> v .! 7 <*> v .! 8
+		| otherwise     = fail "Invalid number of columns"
 
---Alinhar questões do Pokemon com Aelson antes de mudar
-cenaDeBatalhaInterface :: Gtk.Application -> Gtk.ApplicationWindow -> IO ()
-cenaDeBatalhaInterface app w = do
-  Gtk.windowDestroy w
-  box <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
+generateHp :: Int -> Int -> Int
+generateHp base level = 
+	(div ((2 * base + 31) * level) 100) + level + 10
 
-  window <- new Gtk.ApplicationWindow [#application := app,
-                                  #title := "Batalha",
-                                  #child := box,
-                                  #defaultWidth := 800,
-                                  #defaultHeight := 600]
+generateStat :: Int -> Int -> Int
+generateStat base level =
+   (div ((2 * base + 31) * level) 100) + 5
 
-  button1 <- new Gtk.Button [#label := "ATAQUE"]
-  button2 <- new Gtk.Button [#label := "INVENTÁRIO"]
-  button3 <- new Gtk.Button [#label := "POKEMON"]
-  button4 <- new Gtk.Button [#label := "DESISTIR"]
+generatePokemon :: Pokemon -> Int -> String -> String -> String -> String -> PokemonBattle
+generatePokemon pokemon nivel attack1 attack2 attack3 attack4 =
+	let currentHp = generateHp (hp pokemon) nivel
+	    currentFAtk = generateStat (fAtk pokemon) nivel
+	    currentFDef = generateStat (fDef pokemon) nivel 
+	    currentSAtk = generateStat (sAtk pokemon) nivel 
+	    currentSDef = generateStat (sDef pokemon) nivel 
+	    currentSpeed = generateStat (spd pokemon) nivel 
+	    currentName = nome pokemon
+	    pokBattle = PokemonBattle {
+		nomePok = currentName,
+		type1 = tipo1 pokemon,
+		type2 = tipo2 pokemon,
+		maxHitPoints = currentHp,
+		hitPoints = currentHp,
+		fAttack = currentFAtk,
+		fDefense = currentFDef,
+		sAttack = currentSAtk,
+		sDefense = currentSDef,
+		speed = currentSpeed,
+		level = nivel,
+		atk1 = attack1,
+		atk2 = attack2,
+		atk3 = attack3,
+		atk4 = attack4,
+		condition = ""
+	    }
+	in pokBattle
 
-  #append box button1
-  #append box button2
-  #append box button3
-  #append box button4
+decideQuemVaiPrimeiro :: PokemonBattle -> PokemonBattle -> Int
+decideQuemVaiPrimeiro pok1 pok2 = 
+	if (speed pok1) >= (speed pok2)
+		then 1
+		else 2
 
-  window.show
+--calculaCritico :: Int -> IO Bool
+--calculaCritico critical = do
+--	rand <- randomRIO(1, 100)
+--	return (rand <= critical * 4)
 
--- Gera a interface da Escolha do Segundo Pokemon 
--- Encaminha para a cena de batalha
-=======
->>>>>>> 5bc8f2c2fc911aaf4418f31f55cfea0ad0d4c2d8
-escolhaPokemon2 :: Gtk.Application -> Gtk.ApplicationWindow -> IO()
-escolhaPokemon2 app w = do
-  
-  Gtk.windowDestroy w
-  box <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
+calculaRandom :: IO Double
+calculaRandom = do
+	rand <- randomRIO(80, 100)
+	return (rand / 100)
 
-  window <- new Gtk.ApplicationWindow [#application := app,
-                                  #title := "Haskell Gi - Examples - Packed Widgets",
-                                  #child := box,
-                                  #defaultWidth := 800,
-                                  #defaultHeight := 600]
+calculaEficiencia :: String -> String -> Double
+calculaEficiencia tipoAtaque tipoAlvo =
+	1.0
 
-  button1 <- new Gtk.Button [#label := "Pokémon 1",
-<<<<<<< HEAD
-                            On #clicked (cenaDeBatalhaInterface app window)]
-  button2 <- new Gtk.Button [#label := "Pokémon 2",
-                            On #clicked (cenaDeBatalhaInterface app window)]
-  button3 <- new Gtk.Button [#label := "Pokémon 3",
-                            On #clicked (cenaDeBatalhaInterface app window)]
-  button4 <- new Gtk.Button [#label := "Pokémon 4",
-                            On #clicked (cenaDeBatalhaInterface app window)]
-  button5 <- new Gtk.Button [#label := "Pokémon 5",
-                            On #clicked (cenaDeBatalhaInterface app window)]                                                                                                        
-=======
-                            On #clicked cenaDeBatalhaInterface app window]
-  button2 <- new Gtk.Button [#label := "Pokémon 2",
-                            On #clicked cenaDeBatalhaInterface app window]
-  button3 <- new Gtk.Button [#label := "Pokémon 3",
-                            On #clicked cenaDeBatalhaInterface app window]
-  button4 <- new Gtk.Button [#label := "Pokémon 4",
-                            On #clicked cenaDeBatalhaInterface app window]
-  button5 <- new Gtk.Button [#label := "Pokémon 5",
-                            On #clicked cenaDeBatalhaInterface app window]                                                                                                        
->>>>>>> 5bc8f2c2fc911aaf4418f31f55cfea0ad0d4c2d8
-                            
-  #append box button1
-  #append box button2
-  #append box button3
-  #append box button4
-  #append box button5
-<<<<<<< HEAD
+eficiencia :: String -> String -> String -> Double
+eficiencia tipoAtaque tipo1Alvo tipo2Alvo = (calculaEficiencia tipoAtaque tipo1Alvo) * (calculaEficiencia tipoAtaque tipo2Alvo)
 
--- Gera a interface da Escolha do Primeiro Pokemon 
--- Encaminha para a escolha do segundo pokemon
-=======
-   
->>>>>>> 5bc8f2c2fc911aaf4418f31f55cfea0ad0d4c2d8
-escolhaPokemon1 :: Gtk.Application -> Gtk.ApplicationWindow -> IO()
-escolhaPokemon1 app w = do
-  
-  Gtk.windowDestroy w
-  box <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
+realizaAtaque :: PokemonBattle -> PokemonBattle -> Attack ->IO PokemonBattle
+realizaAtaque atacante alvo ataque = do
+	resultado <- calculaAcerto (accuracy ataque)
+	if resultado
+		then return newPokemon
+		else return alvo
+	where atq = if (category ataque) == "Physical" then fAttack atacante else sAttack atacante
+	      def = if (category ataque) == "Physical" then fDefense alvo else sDefense alvo
+	      stab = if ((typing ataque) == (type1 atacante) || (typing ataque) == (type2 atacante)) then 1.5 else 1.0
+	      efficiency = eficiencia (typing ataque) (type1 alvo) (type2 alvo)
+	      burn = if ((condition atacante) == "Burning" && (category ataque) == "Physical") then 0.5 else 1.0
+	      dano = calculaDano (level atacante) (power ataque) atq def stab efficiency burn
+	      newPokemon = alteraHP alvo dano 
 
-  window <- new Gtk.ApplicationWindow [#application := app,
-                                  #title := "Haskell Gi - Examples - Packed Widgets",
-                                  #child := box,
-                                  #defaultWidth := 800,
-                                  #defaultHeight := 600]
+	
+calculaDano :: Int -> Int -> Int -> Int -> Double -> Double -> Double -> Int
+calculaDano level poder ataque defesa stab tipo burn = 
+     truncate (((((((fromIntegral level) * 2 / 5) + 2) * (fromIntegral poder ) * ((fromIntegral ataque) / (fromIntegral defesa))) / 50) + 2) * stab * tipo * burn)
+ 
+utilizaItem :: PokemonBattle -> Item -> PokemonBattle
+utilizaItem pokemon item = 
+	if (itemName == "HyperPotion")
+		then newPokemon
+	else if (itemName == "FullRestore")
+		then pokemon
+	else if (itemName == "Antidote")
+		then pokemon
+	else if (itemName == "BurnHeal")
+		then pokemon
+	else if (itemName == "ParalyzeHeal")
+		then pokemon
+	else if (itemName == "Awakening")
+		then pokemon
+	else if (itemName == "IceHeal") 
+		then pokemon
+	else pokemon
+	where itemName = (nomeItem item)
+	      newPokemon = alteraHP pokemon 120 
+	
 
-  button1 <- new Gtk.Button [#label := "Pokémon 1",
-                            On #clicked (escolhaPokemon2 app window)]
-  button2 <- new Gtk.Button [#label := "Pokémon 2",
-                            On #clicked (escolhaPokemon2 app window)]
-  button3 <- new Gtk.Button [#label := "Pokémon 3",
-                            On #clicked (escolhaPokemon2 app window)]
-  button4 <- new Gtk.Button [#label := "Pokémon 4",
-                            On #clicked (escolhaPokemon2 app window)]
-  button5 <- new Gtk.Button [#label := "Pokémon 5",
-                            On #clicked (escolhaPokemon2 app window)]                                                                                                        
+calculaAcerto :: Int -> IO Bool
+calculaAcerto accuracy = do
+	rand <- randomRIO(1, 100)
+	return (rand <= accuracy)
 
-  #append box button1
-  #append box button2
-  #append box button3
-  #append box button4
-  #append box button5
+calculaHp :: Int -> Int -> Int -> Int
+calculaHp hpMax hpCurr vida = 
+	if (hpCurr + vida) <= 0
+		then 0
+		else if (hpCurr + vida) >= hpMax
+			then hpMax
+			else (hpCurr + vida)
 
+alteraHP :: PokemonBattle -> Int -> PokemonBattle
+alteraHP pokemon vida =
+	let newHp = calculaHp (maxHitPoints pokemon) (hitPoints pokemon) vida
+        in pokemon { hitPoints = newHp}
 
-<<<<<<< HEAD
--- Gera a interface Inicial 
--- Encaminha para a escolha do primeiro pokemon
-=======
-
->>>>>>> 5bc8f2c2fc911aaf4418f31f55cfea0ad0d4c2d8
-escolhaSeuPokemonInterface :: Gtk.Application -> Gtk.ApplicationWindow -> IO ()
-escolhaSeuPokemonInterface app w = do
-  Gtk.windowDestroy w
-
-  box <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
-  
-  window <- new Gtk.ApplicationWindow [ #application := app,
-                                  #title := "Haskell Gi - Examples - Packed Widgets",
-                                  #child := box,
-                                  #defaultWidth := 800,
-                                  #defaultHeight := 600
-                                 ]
-  button1 <- new Gtk.Button [#label := "Pokémon 1",
-                            On #clicked (escolhaPokemon1 app window)]
-  button2 <- new Gtk.Button [#label := "Pokémon 2",
-                            On #clicked (escolhaPokemon1 app window)]
-  button3 <- new Gtk.Button [#label := "Pokémon 3",
-                            On #clicked (escolhaPokemon1 app window)]
-  button4 <- new Gtk.Button [#label := "Pokémon 4",
-                            On #clicked (escolhaPokemon1 app window)]
-  button5 <- new Gtk.Button [#label := "Pokémon 5",
-                            On #clicked (escolhaPokemon1 app window)]                                                                                                        
-
-  #append box button1
-  #append box button2
-  #append box button3
-  #append box button4
-  #append box button5
-
-  Gtk.widgetSetValign box Gtk.AlignCenter
-
-<<<<<<< HEAD
-=======
-  window.show
-
-
-
-                               
-
-cenaDeBatalhaInterface :: Gtk.Application -> Gtk.ApplicationWindow
-cenaDeBatalhaInterface app w = do
-  Gtk.windowDestroy w
-  box <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
-
-  window <- new Gtk.ApplicationWindow [#application := app,
-                                  #title := "Batalha",
-                                  #child := box,
-                                  #defaultWidth := 800,
-                                  #defaultHeight := 600]
-
-  button1 <- new Gtk.Button [#label := "ATAQUE",
-                            On #clicked print ""]
-  button2 <- new Gtk.Button [#label := "INVENTÁRIO",
-                            On #clicked print ""]
-  button3 <- new Gtk.Button [#label := "POKEMON",
-                            On #clicked print ""]
-  button4 <- new Gtk.Button [#label := "DESISTIR",
-                            On #clicked print ""]
-
-  #append box button1
-  #append box button2
-  #append box button3
-  #append box button4
-
->>>>>>> 5bc8f2c2fc911aaf4418f31f55cfea0ad0d4c2d8
-  window.show
-
-
-
-                               
-
-
---Inicio da Aplicação
-activate :: Gtk.Application -> IO ()
-activate app = do
-  box <- new Gtk.Box [#orientation := Gtk.OrientationHorizontal]
-
-
-  window <- new Gtk.ApplicationWindow [#application := app,
-                                       #title := "Pokemon-Haskell",
-                                       #child := box,
-                                       #defaultWidth := 800,
-                                       #defaultHeight := 600]
-
-  button <- new Gtk.Button [#label := "Jogar",
-                            On #clicked (escolhaSeuPokemonInterface app window) ]
-  #append box button
-  Gtk.widgetSetValign box Gtk.AlignCenter  -- Alinha horizontalmente no centro
-  Gtk.widgetSetHalign box Gtk.AlignCenter  -- Alinha verticalmente no centro              
-  window.show
-
-main :: IO ()
+main::IO()
 main = do
-  app <- new Gtk.Application [#applicationId := "haskell-gi.example",
-                              On #activate (activate ?self)]
-
-  void $ app.run Nothing
+	csvData <- BL.readFile "./data/pokemon.csv"
+	let decoded = decode HasHeader csvData :: Either String (Vector Pokemon)
+	case decoded of
+        	Left err  -> putStrLn $ "Error parsing CSV: " ++ err
+        	Right pokemons -> mapM_ print pokemons
+	
