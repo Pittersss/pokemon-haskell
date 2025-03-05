de85b6fd8881870a3800ea44b4991c3e1a23bfb7
Pedro Malaquias <pedromalaquias608@gmail.com>
2025-03-05 13:31:43 -0300

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -17,25 +17,21 @@ import Data.GI.Base
 import Data.Text (Text, pack)
 import GHC.Word (Word32)
 import qualified Data.ByteString.Char8 as BS
-import Battle
 import System.IO.Unsafe (unsafePerformIO)
 
 onButtonClicked :: Gtk.Application -> IORef [Text] -> Text -> Gtk.ApplicationWindow -> IO ()
 onButtonClicked app selectedButtons name window = do
-    modifyIORef selectedButtons (\selected -> take 2 (selected ++ [name]))
+    modifyIORef selectedButtons (\selected -> take 6 (selected ++ [name]))
     selection <- readIORef selectedButtons
-    if length selection == 2
+    if length selection == 6
         then do
-              print(selection)
-              print((selection !! 0) == "charizard")
               Gtk.windowDestroy window
               battleCeneInterface app selection
 
         else pure ()
 
-choicePokemonInterface :: Gtk.Application -> Gtk.ApplicationWindow -> IO ()
-choicePokemonInterface app w = do
-  Gtk.windowDestroy w
+choicePokemonInterfaceW :: Gtk.Application -> IO ()
+choicePokemonInterfaceW app = do
 
   window <- new Gtk.ApplicationWindow [
       #application   := app,
@@ -60,7 +56,7 @@ choicePokemonInterface app w = do
     ]
 
   bgImage <- new Gtk.Image [
-      #file   := "images/background1.jpeg",
+      #file   := "/images/background2.jpg",
       #name   := "image",
       #hexpand:= True,
       #vexpand:= True
@@ -76,7 +72,7 @@ choicePokemonInterface app w = do
     ]
 
   titleLabel <- new Gtk.Label [
-      #label  := "Selecione seu Pokémon",
+      #label  := "Selecione seus Pokémons",
       #name   := "titleLabel",
       #halign := Gtk.AlignCenter
     ]
@@ -91,16 +87,22 @@ choicePokemonInterface app w = do
   Gtk.gridSetColumnSpacing grid 7
 
   selectedButtons <- newIORef ([] :: [Text])
-  let spriteSheetPath = "images/spritesheet.png"
-  window.show
+  let spriteSheetPath = "images/spritesheettemp.png"
+
   let spriteMapping :: [(Text, (Int, Int, Int, Int))]
       spriteMapping =
-         [ ("charizard", (0, 394, 32, 32))
-         , ("blastoise", (0, 644, 32, 32))
-         , ("venusaur", (0, 160, 32, 32))
-         , ("pikachu", (0, 1744, 24, 32))
-         , ("pidgeot", (0, 1184, 22, 32))
-         , ("butterfree", (0, 832, 30, 32))
+         [ ("Charizard", (0, 394, 32, 32))
+         , ("Blastoise", (0, 644, 32, 32))
+         , ("Venusaur", (0, 160, 32, 32))
+         , ("Pikachu", (0, 1744, 24, 32))
+         , ("Pidgeot", (0, 1184, 22, 32))
+         , ("Butterfree", (0, 832, 30, 32))
+         , ("Alakazam", (1224,1050,27,32))
+         , ("Gengar", (619,1298,22,32))
+         , ("Onix", (619,1370,25,60))
+         , ("Seadra", (2440,1380,30,32))
+         , ("Hitmonlee", (2446,490,32,32))
+         , ("Cloyster", (618,1084,25,32))
          ]
 
   btns <- mapM (\(name, (x, y, w, h)) -> do
@@ -130,7 +132,115 @@ choicePokemonInterface app w = do
   convertAndApplyStyle titleLabel "style/selectionwindow.css"
 
   #setChild window (Just overlay)
-  #show window          
+  #show window
+
+-----
+
+choicePokemonInterface :: Gtk.Application -> Gtk.ApplicationWindow -> IO ()
+choicePokemonInterface app w = do
+  Gtk.windowDestroy w
+
+  window <- new Gtk.ApplicationWindow [
+      #application   := app,
+      #name          := "window",
+      #defaultWidth  := 800,
+      #defaultHeight := 600
+    ]
+
+  overlay <- new Gtk.Overlay [
+      #name   := "overlay",
+      #hexpand := True,
+      #vexpand := True
+    ]
+
+  container <- new Gtk.Box [
+      #orientation := Gtk.OrientationVertical,
+      #name        := "backgroundContainer",
+      #hexpand     := True,
+      #vexpand     := True,
+      #halign      := Gtk.AlignCenter,
+      #valign      := Gtk.AlignCenter
+    ]
+
+  bgImage <- new Gtk.Image [
+      #file   := "/images/background2.jpg",
+      #name   := "image",
+      #hexpand:= True,
+      #vexpand:= True
+    ]
+
+  #append container bgImage
+
+  labelBox <- new Gtk.Box [
+      #name        := "labelBox",
+      #orientation := Gtk.OrientationVertical,
+      #halign      := Gtk.AlignCenter,
+      #valign      := Gtk.AlignCenter
+    ]
+
+  titleLabel <- new Gtk.Label [
+      #label  := "Selecione seus Pokémons",
+      #name   := "titleLabel",
+      #halign := Gtk.AlignCenter
+    ]
+
+  grid <- new Gtk.Grid [
+      #name   := "buttonGrid",
+      #halign := Gtk.AlignCenter,
+      #valign := Gtk.AlignCenter
+    ]
+
+  Gtk.gridSetRowSpacing grid 7
+  Gtk.gridSetColumnSpacing grid 7
+
+  selectedButtons <- newIORef ([] :: [Text])
+  let spriteSheetPath = "images/spritesheettemp.png"
+
+  let spriteMapping :: [(Text, (Int, Int, Int, Int))]
+      spriteMapping =
+         [ ("Charizard", (0, 394, 32, 32))
+         , ("Blastoise", (0, 644, 32, 32))
+         , ("Venusaur", (0, 160, 32, 32))
+         , ("Pikachu", (0, 1744, 24, 32))
+         , ("Pidgeot", (0, 1184, 22, 32))
+         , ("Butterfree", (0, 832, 30, 32))
+         , ("Alakazam", (1224,1050,27,32))
+         , ("Gengar", (619,1298,22,32))
+         , ("Onix", (619,1370,25,60))
+         , ("Seadra", (2440,1380,30,32))
+         , ("Hitmonlee", (2446,490,32,32))
+         , ("Cloyster", (618,1084,25,32))
+         ]
+
+  btns <- mapM (\(name, (x, y, w, h)) -> do
+    btn <- new Gtk.Button []
+    let buttonName = "button_" <> name
+    #setName btn buttonName
+    maybeImage <- loadSprite spriteSheetPath x y w h
+    case maybeImage of
+        Just img -> #setChild btn (Just img)
+        Nothing  -> putStrLn ("Erro ao carregar sprite " ++ show name)
+    void $ on btn #clicked (onButtonClicked app selectedButtons name window)
+    convertAndApplyStyle btn "style/selectionwindow.css"
+    return btn) spriteMapping
+
+  mapM_ (\(btn, idx) -> #attach grid btn (fromIntegral ((idx - 1) `mod` 3)) (fromIntegral ((idx - 1) `div` 3)) 1 1) (zip btns [1..length btns])
+
+  #append labelBox titleLabel
+  #append labelBox grid
+
+  #addOverlay overlay container
+  #addOverlay overlay labelBox
+
+  convertAndApplyStyle overlay "style/selectionwindow.css"
+  convertAndApplyStyle container "style/selectionwindow.css"
+  convertAndApplyStyle labelBox "style/selectionwindow.css"
+  convertAndApplyStyle grid "style/selectionwindow.css"
+  convertAndApplyStyle titleLabel "style/selectionwindow.css"
+
+  #setChild window (Just overlay)
+  #show window
+          
 
 
 battleCeneInterface :: Gtk.Application -> [Text] -> IO ()
@@ -169,8 +279,8 @@ battleCeneInterface app ref = do
                           
 
   button1 <- new Gtk.Button [#label := "ATAQUE",
-                             #name := "atq_btn",
-                             On #clicked (extractAllPokemons ref)] 
+                             #name := "atq_btn"
+                            ] 
 
   button2 <- new Gtk.Button [#label := "POKEMON",
                              #name := "pokemon_btn"]
@@ -223,60 +333,144 @@ battleCeneInterface app ref = do
   convertAndApplyStyle button3 "style/battlecene.css"
   convertAndApplyStyle button4 "style/battlecene.css"
   convertAndApplyStyle bgImage "style/battlecene.css"
+
+  --window.show
+
+  pkbUsu <- geraPokemonsUsuario ref
+  pkbEn <- geraPokemonsUsuarioS ["Cloyster", "Onix", "Gengar", "Dragonite", "Pidgeot"]
+ 
+
+  batalhaI pkbUsu pkbEn ["Aelson", "Beatriz", "Luana", "Pedro", "Everton"] app
+
   
-  let pokemonsUsu = generateBasePokemon ref
-  let pokemonsBtlUsu = generateBasePokemonBattle pokemonsUsu []
+infoBtl :: PokemonBattle -> PokemonBattle -> [Item] -> IO ()
+infoBtl usuPkmn enPkmn itens = do
+	putStrLn ""
+	putStrLn "=============================="
+	putStrLn $ "Seu Pokemon: " ++ nome (pkmn usuPkmn)
+	putStrLn $ "Current HP: " ++ show (currentHp usuPkmn)
+	putStrLn $ "Condição: " ++ (condition usuPkmn)
+	putStrLn $ "Hyper Potions: " ++ show (qtde (head itens))
+	putStrLn $ "Full Restore: " ++ show (qtde (head (tail itens)))
+	putStrLn ""
+	putStrLn "=============================="
+	putStrLn $ "Pokemon do adversário: " ++ nome (pkmn enPkmn)
+	putStrLn $ "Current HP: " ++ show (currentHp enPkmn)
+	putStrLn $ "Condição: " ++ (condition usuPkmn)
+	putStrLn ""
 
-  let pokemonEn = pokemonsInimigo "cloyster"
-  let pokemonBtlEn = pokemonsBatalhaInimigo pokemonEn
+pickAttack :: PokemonBattle -> Int -> String
+pickAttack pokemon numAtk = 
+    if (numAtk == 1) then (name (atk1 pokemon))
+    else if (numAtk == 2) then (name (atk2 pokemon))
+    else if (numAtk == 3) then (name (atk3 pokemon))
+    else name (atk4 pokemon)
 
-  batalhaUm pokemonsBtlUsu pokemonBtlEn 0 app window
+imprimeAtaques :: PokemonBattle -> IO ()
+imprimeAtaques pokemon = do
+	putStrLn $ "=============================="
+	putStrLn $ (("1." ++ (attack1 (pkmn pokemon))) ++ ", ") ++ (show (pp (atk1 pokemon)) ++ "/" ++ show (maxPP (atk1 pokemon)))
+	putStrLn $ (("2." ++ (attack2 (pkmn pokemon))) ++ ", ") ++ (show (pp (atk2 pokemon)) ++ "/" ++ show (maxPP (atk2 pokemon)))
+	putStrLn $ (("3." ++ (attack3 (pkmn pokemon))) ++ ", ") ++ (show (pp (atk3 pokemon)) ++ "/" ++ show (maxPP (atk3 pokemon)))
+	putStrLn $ (("4." ++ (attack4 (pkmn pokemon))) ++ ", ") ++ (show (pp (atk4 pokemon)) ++ "/" ++ show (maxPP (atk4 pokemon)))
+        putStrLn $ "=============================="
 
-  window.show
 
-converteIOparaPokemonBattle :: IO PokemonBattle -> PokemonBattle
-converteIOparaPokemonBattle ioPkmnBattle = unsafePerformIO ioPkmnBattle
+batalhaI :: [PokemonBattle] -> [PokemonBattle] -> [String] -> Gtk.Application -> IO ()
+batalhaI usuPkmns enPkmns rivais app = do
+	let hyperPotions = Item {nomeItem = "Hyper Potion", qtde = 5}
+	let fullRestores = Item {nomeItem = "Full Restore", qtde = 5}
+	let listaItens = [hyperPotions, fullRestores]
+	batalha usuPkmns enPkmns rivais listaItens app
+  --putStrLn("Acabou a partida!")
 
-swapFirst :: Int -> [Pokemon] -> [Pokemon]
-swapFirst _ [] = []
-swapFirst _ [x] = [x]
-swapFirst 0 (x:xs) = (x:xs)
-swapFirst i (x:xs)
-  | i < 0 || i >= length (x:xs) = error "Índice fora dos limites"
-  | otherwise = (xs !! (i - 1)) : take (i - 1) xs ++ [x] ++ drop i xs
-
-batalhaUm :: [PokemonBattle] -> PokemonBattle -> Int -> Gtk.Application -> Gtk.ApplicationWindow -> IO ()
-batalhaUm usuPkmns enPkmn turno app window = do 
-  if (currentHp (usuPkmns !! 0) <= 0) then
-    batalhaUm (tail usuPkmns) enPkmn turno app window
-  else if (currentHp enPkmn) <= 0 then do
-    let pokemonEn = pokemonsInimigo "charizard"
-    let pokemonBtlEn = pokemonsBatalhaInimigo pokemonEn
-    batalhaDois usuPkmns pokemonBtlEn 0 app window
-  else if turno == 0 then do
-    print("Pokemon Atual: " ++ (nome (pkmn (usuPkmns !! 0))))
-    print("Pokemon Adversário: " ++ (nome (pkmn enPkmn)))
-    let primeiroTurno = decideQuemVaiPrimeiro (usuPkmns !! 0) enPkmn
-    batalhaUm usuPkmns enPkmn primeiroTurno app window
-  else if turno == 1 then do
-    print("Digite o ataque escolhido")
-    mostraAtaques (usuPkmns !! 0)
-    atkEscolhido <- readLn :: IO Int
-    let enNewState = converteIOparaPokemonBattle (realizaAtaque (usuPkmns !! 0) enPkmn atkEscolhido)
-    print((currentHp (usuPkmns !! 0)))
-    print((currentHp enNewState))
-    batalhaUm usuPkmns enNewState 2 app window
-  else if turno == 2 then do
-    -- lógica do ataque random vai ser implementada ainda
-    print("O inimigo usou: " ++ (name (atk1 enPkmn)))
-    let usuNewState = converteIOparaPokemonBattle (realizaAtaque enPkmn (usuPkmns !! 0) 1)
-    print((currentHp usuNewState))
-    print((currentHp enPkmn))
-    batalhaUm ([usuNewState] ++ (tail usuPkmns)) enPkmn 1 app window
-  else print("")
-
-batalhaDois :: [PokemonBattle] -> PokemonBattle -> Int -> Gtk.Application -> Gtk.ApplicationWindow -> IO ()
-batalhaDois a b c d e = print("")
+batalha :: [PokemonBattle] -> [PokemonBattle] -> [String] -> [Item] -> Gtk.Application -> IO ()
+batalha usuPkmns enPkmns rivais itens app
+	|null usuPkmns = do
+		--putStrLn("Você perdeu, tente novamente")
+    choicePokemonInterfaceW app
+	| null enPkmns = do
+		--putStrLn("Você agora você é o campeão da Elite 4")
+    Gio.applicationQuit app
+	| currentHp (head usuPkmns) <= 0 = do
+		putStrLn $ "Seu pokemon desmaiou, se houver um próximo pokemon, ele entrará no lugar"
+		batalha (tail usuPkmns) enPkmns rivais itens app
+	| currentHp (head enPkmns) <= 0 = do
+		putStrLn $ "Parabéns, você derrotou " ++ (head rivais)
+		batalha usuPkmns (tail enPkmns) (tail rivais) itens app
+	| otherwise = do
+		infoBtl (head usuPkmns) (head enPkmns) itens	
+		putStrLn $ "O que você deseja fazer?"
+		putStrLn $ "1. Usar uma Hyper Potion, 2. Usar um Full Restore ou 3. Atacar"
+		decision <- readLn :: IO Int
+		melhorAtaque <- escolheMelhorAtaque (head enPkmns) (head usuPkmns)
+		if decision == 1
+			then do
+				if qtde (head itens) > 0
+					then do let tupla = utilizaItem (head usuPkmns) (head itens)
+						let novoPokemon = fst tupla
+						let novoItem = snd tupla
+						(novoAtacante, novoAlvo) <- realizaAtaque (head enPkmns) novoPokemon melhorAtaque
+						let newUsuPkmns = [novoAlvo] ++ (tail usuPkmns)
+						let newEnPkmns = [novoAtacante] ++ (tail enPkmns)
+						let newItens = [novoItem] ++ (tail itens)
+						putStrLn $ "O pokemon adversário usou o golpe " ++ (pickAttack (head enPkmns) melhorAtaque)
+						batalha newUsuPkmns newEnPkmns rivais newItens app
+				else do
+					putStrLn "Acabaram as Hyper Potions. Faça outra coisa"
+					batalha usuPkmns enPkmns rivais itens app
+		else if decision == 2
+			then do
+				if qtde (head (tail itens)) > 0
+					then do let tupla = utilizaItem (head usuPkmns) (head (tail itens))
+						let novoPokemon = fst tupla
+						let novoItem = snd tupla
+						(novoAtacante, novoAlvo) <- realizaAtaque (head enPkmns) novoPokemon melhorAtaque
+						let newUsuPkmns = [novoAlvo] ++ (tail usuPkmns)
+						let newEnPkmns = [novoAtacante] ++ (tail enPkmns)
+						let newItens = [head itens] ++ [novoItem]
+						putStrLn $ "O pokemon adversário usou o golpe " ++ (pickAttack (head enPkmns) melhorAtaque)
+						batalha newUsuPkmns newEnPkmns rivais newItens app
+				else do
+					putStrLn "Acabaram os Full Restore. Faça outra coisa"
+					batalha usuPkmns enPkmns rivais itens app
+		else do
+			putStrLn $ ""
+			putStrLn $ "Qual ataque você deseja usar? "
+			imprimeAtaques (head usuPkmns)
+			escolha <- readLn :: IO Int
+			let aux = decideQuemVaiPrimeiro (head usuPkmns) (head enPkmns)
+			if (escolha >= 1 && escolha <= 4 && aux == 1 && (temPP (head usuPkmns) escolha)) 
+				then do
+					putStrLn $ "Seu pokemon ataca primeiro" 
+					(novoAtacante, novoAlvo) <- realizaAtaque (head usuPkmns) (head enPkmns) escolha
+					let newUsuPkmns = [novoAtacante] ++ (tail usuPkmns)
+					let newEnPkmns = [novoAlvo] ++ (tail enPkmns)
+					if currentHp (head newEnPkmns) <= 0
+						then do batalha newUsuPkmns newEnPkmns rivais itens app
+						else do
+							print "O pokemon do adversario ataca de volta"
+							(novoPkmnEn, novoPkmnUsu) <- realizaAtaque (head newEnPkmns) (head newUsuPkmns) melhorAtaque
+							let newUsuPkmns2 = [novoPkmnUsu] ++ (tail newUsuPkmns)
+							let newEnPkmns2 = [novoPkmnEn] ++ (tail newEnPkmns)
+							batalha newUsuPkmns2 newEnPkmns2 rivais itens app
+			else if (escolha >= 1 && escolha <= 4 && aux == 2 && (temPP (head usuPkmns) escolha))
+				then do
+					print "O pokemon do adversario ataca primeiro"
+					(novoAtacante, novoAlvo) <- realizaAtaque (head enPkmns) (head usuPkmns) melhorAtaque
+					let newUsuPkmns = [novoAlvo] ++ (tail usuPkmns)
+					let newEnPkmns = [novoAtacante] ++ (tail enPkmns)
+					if currentHp (head newUsuPkmns) <= 0
+						then do batalha newUsuPkmns newEnPkmns rivais itens app
+						else do
+							print "O seu pokemon ataca de volta"
+							(novoPkmnUsu, novoPkmnEn) <- realizaAtaque (head newUsuPkmns) (head newEnPkmns) escolha
+							let newUsuPkmns2 = [novoPkmnUsu] ++ (tail newUsuPkmns)
+							let newEnPkmns2 = [novoPkmnEn] ++ (tail newEnPkmns)
+							batalha newUsuPkmns2 newEnPkmns2 rivais itens app
+			else do 
+				putStrLn $ "Entrada inválida"
+				batalha usuPkmns enPkmns rivais itens app
 
 --Inicio da Aplicação
 activate :: Gtk.Application -> IO ()
