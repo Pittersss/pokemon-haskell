b166a87a7be39f5e98b636204126daf47cf17422
aelsonjrdiniz <aelson.junior.araujo.diniz.cunha@ccc.ufcg.edu.br>
2025-03-05 11:45:06 -0300

    Terminando

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import Control.Applicative
 import System.Random (randomRIO)
 import Control.Monad 
 import Data.Text (Text, unpack, pack)
+import System.IO
 
 data Pokemon = Pokemon {
 	nome :: String,
@@ -186,13 +187,17 @@ escolheMelhorAtaque :: PokemonBattle -> PokemonBattle -> IO Int
 escolheMelhorAtaque atacante alvo = do
 	let typing1 = (tipo1 (pkmn alvo))
 	let typing2 = (tipo2 (pkmn alvo))
-	if (ehSuperEfetivo (typing (atk1 atacante)) typing1 typing2) then return 1
-	else if (ehSuperEfetivo (typing (atk2 atacante)) typing1 typing2) then return 2
-	else if (ehSuperEfetivo (typing (atk3 atacante)) typing1 typing2) then return 3
-	else if (ehSuperEfetivo (typing (atk4 atacante)) typing1 typing2) then return 4
+	if (ehSuperEfetivo (typing (atk1 atacante)) typing1 typing2 && (temPP atacante 1)) then return 1
+	else if (ehSuperEfetivo (typing (atk2 atacante)) typing1 typing2 && (temPP atacante 2)) then return 2
+	else if (ehSuperEfetivo (typing (atk3 atacante)) typing1 typing2 && (temPP atacante 3)) then return 3
+	else if (ehSuperEfetivo (typing (atk4 atacante)) typing1 typing2 && (temPP atacante 4)) then return 4
 	else do
 		aux <- randomRIO(1,4) :: IO Int
-		return aux
+		if (temPP atacante aux) then return aux
+		else if (temPP atacante 1) then return 1
+		else if (temPP atacante 2) then return 2
+		else if (temPP atacante 3) then return 3
+		else return 4
 
 realizaAtaque :: PokemonBattle -> PokemonBattle -> Int -> IO (PokemonBattle,PokemonBattle)
 realizaAtaque atacante alvo numAtaque = do
@@ -259,11 +264,11 @@ calculaDano poder ataque defesa stab tipo burn critico =
      truncate ((((((50 * 2 / 5) + 2) * (fromIntegral poder ) * ((fromIntegral ataque) / (fromIntegral defesa))) / 50) + 2) * stab * tipo * burn * critico)
 
 utilizaItem :: PokemonBattle -> Item -> (PokemonBattle, Item)
-utilizaItem pokemon item = 
-	let newQtde = (qtde item) - 1 in
-	if (itemName == "HyperPotion" && newQtde > 0)
+utilizaItem pokemon item = do
+	let newQtde = (qtde item) - 1
+	if (itemName == "Hyper Potion" && (qtde item) > 0)
 		then (newPokemonH, item {qtde = newQtde})
-	else if (itemName == "Full Restore" && newQtde > 0) 
+	else if (itemName == "Full Restore" && (qtde item) > 0) 
 		then (newPokemonF, item {qtde = newQtde})
 	else (pokemon, item)
 	where itemName = (nomeItem item)
@@ -380,17 +385,181 @@ geraPokemonsUsuario pokemons = do
 		lista <- geraPokemonsUsuario (tail pokemons)
 		return ([pkmnBattle] ++ lista)	
 
+infoBtl :: PokemonBattle -> PokemonBattle -> [Item] -> IO ()
+infoBtl usuPkmn enPkmn itens = do
+	putStrLn ""
+	putStrLn "=============================="
+	putStrLn $ "Seu Pokemon: " ++ nome (pkmn usuPkmn)
+	putStrLn $ "Current HP: " ++ show (currentHp usuPkmn)
+	putStrLn $ "Condição: " ++ (condition usuPkmn)
+	putStrLn $ "Hyper Potions: " ++ show (qtde (head itens))
+	putStrLn $ "Full Restore: " ++ show (qtde (head (tail itens)))
+	putStrLn ""
+	putStrLn "=============================="
+	putStrLn $ "Pokemon do adversário: " ++ nome (pkmn enPkmn)
+	putStrLn $ "Current HP: " ++ show (currentHp enPkmn)
+	putStrLn $ "Condição: " ++ (condition usuPkmn)
+	putStrLn ""
+
+imprimeAtaques :: PokemonBattle -> IO ()
+imprimeAtaques pokemon = do
+	putStrLn $ "=============================="
+	putStrLn $ (("1." ++ (attack1 (pkmn pokemon))) ++ ", ") ++ (show (pp (atk1 pokemon)) ++ "/" ++ show (maxPP (atk1 pokemon)))
+	putStrLn $ (("2." ++ (attack2 (pkmn pokemon))) ++ ", ") ++ (show (pp (atk2 pokemon)) ++ "/" ++ show (maxPP (atk2 pokemon)))
+	putStrLn $ (("3." ++ (attack3 (pkmn pokemon))) ++ ", ") ++ (show (pp (atk3 pokemon)) ++ "/" ++ show (maxPP (atk3 pokemon)))
+	putStrLn $ (("4." ++ (attack4 (pkmn pokemon))) ++ ", ") ++ (show (pp (atk4 pokemon)) ++ "/" ++ show (maxPP (atk4 pokemon)))
+        putStrLn $ "=============================="
+
+pickAttack :: PokemonBattle -> Int -> String
+pickAttack pokemon numAtk = 
+	if (numAtk == 1) then (name (atk1 pokemon))
+	else if (numAtk == 2) then (name (atk2 pokemon))
+	else if (numAtk == 3) then (name (atk3 pokemon))
+	else name (atk4 pokemon)
+
+pegaParte :: [a] -> Int -> Int -> Int -> [a]
+pegaParte lista i j index =
+	if index < i then pegaParte (tail lista) i j (index + 1)
+	else if index > j then []
+	else if (index >= i && index <= j) then ([head lista] ++ (pegaParte (tail lista) i j (index + 1)))
+	else []
+
+particionaLista :: [PokemonBattle] -> Int -> Int -> [[PokemonBattle]]
+particionaLista lista i j = [a, b, c]
+	where a = pegaParte lista 0 (i-1) 0
+	      b = pegaParte lista i j 0
+	      c = pegaParte lista (j+1) ((length lista)-1) 0
+
+trocaHeadTail :: [PokemonBattle] -> [[PokemonBattle]]
+trocaHeadTail pokemons = do
+	let tamanho = length pokemons
+	let a = drop (tamanho -1) pokemons
+	let b = take (tamanho -2) (tail pokemons)
+	let c = head pokemons
+	return (a ++ b ++ [c])
+
+--swap :: [PokemonBattle] -> Int -> Int -> [[PokemonBattle]]
+--swap lista i j = do 
+--	let newLista = particionaLista lista i j
+--	return newLista
+--	let lista1 = lista !! 0
+--	let lista2 = lista !! 1
+--	let lista3 = lista !! 2
+	--let newLista2 = trocaHeadTail lista2
+--	return (lista1 ++ lista3)
+
+batalhaI :: [PokemonBattle] -> [PokemonBattle] -> [String] -> IO ()
+batalhaI usuPkmns enPkmns rivais = do
+	let hyperPotions = Item {nomeItem = "Hyper Potion", qtde = 5}
+	let fullRestores = Item {nomeItem = "Full Restore", qtde = 5}
+	let listaItens = [hyperPotions, fullRestores]
+	batalha usuPkmns enPkmns rivais listaItens
+
+batalha :: [PokemonBattle] -> [PokemonBattle] -> [String] -> [Item] -> IO ()
+batalha usuPkmns enPkmns rivais itens 
+	| null usuPkmns = do
+		putStrLn $ "Você perdeu, tente novamente"
+--              choicePokemonInterfaceW app
+	| null enPkmns = do
+		putStrLn $ "Você agora você é o campeão da Elite 4"
+--              Gio.applicationQuit app
+	| currentHp (head usuPkmns) <= 0 = do
+		putStrLn $ "Seu pokemon desmaiou, se houver um próximo pokemon, ele entrará no lugar"
+		batalha (tail usuPkmns) enPkmns rivais itens
+	| currentHp (head enPkmns) <= 0 = do
+		putStrLn $ "Parabéns, você derrotou " ++ (head rivais)
+		batalha usuPkmns (tail enPkmns) (tail rivais) itens
+	| otherwise = do
+		infoBtl (head usuPkmns) (head enPkmns) itens	
+		putStrLn $ "O que você deseja fazer?"
+		putStrLn $ "1. Usar uma Hyper Potion, 2. Usar um Full Restore ou 3. Atacar"
+		decision <- readLn :: IO Int
+		melhorAtaque <- escolheMelhorAtaque (head enPkmns) (head usuPkmns)
+		if decision == 1
+			then do
+				if qtde (head itens) > 0
+					then do let tupla = utilizaItem (head usuPkmns) (head itens)
+						let novoPokemon = fst tupla
+						let novoItem = snd tupla
+						(novoAtacante, novoAlvo) <- realizaAtaque (head enPkmns) novoPokemon melhorAtaque
+						let newUsuPkmns = [novoAlvo] ++ (tail usuPkmns)
+						let newEnPkmns = [novoAtacante] ++ (tail enPkmns)
+						let newItens = [novoItem] ++ (tail itens)
+						putStrLn $ "O pokemon adversário usou o golpe " ++ (pickAttack (head enPkmns) melhorAtaque)
+						batalha newUsuPkmns newEnPkmns rivais newItens
+				else do
+					putStrLn "Acabaram as Hyper Potions. Faça outra coisa"
+					batalha usuPkmns enPkmns rivais itens
+		else if decision == 2
+			then do
+				if qtde (head (tail itens)) > 0
+					then do let tupla = utilizaItem (head usuPkmns) (head (tail itens))
+						let novoPokemon = fst tupla
+						let novoItem = snd tupla
+						(novoAtacante, novoAlvo) <- realizaAtaque (head enPkmns) novoPokemon melhorAtaque
+						let newUsuPkmns = [novoAlvo] ++ (tail usuPkmns)
+						let newEnPkmns = [novoAtacante] ++ (tail enPkmns)
+						let newItens = [head itens] ++ [novoItem]
+						putStrLn $ "O pokemon adversário usou o golpe " ++ (pickAttack (head enPkmns) melhorAtaque)
+						batalha newUsuPkmns newEnPkmns rivais newItens
+				else do
+					putStrLn "Acabaram os Full Restore. Faça outra coisa"
+					batalha usuPkmns enPkmns rivais itens
+		else do
+			putStrLn $ ""
+			putStrLn $ "Qual ataque você deseja usar? "
+			imprimeAtaques (head usuPkmns)
+			escolha <- readLn :: IO Int
+			let aux = decideQuemVaiPrimeiro (head usuPkmns) (head enPkmns)
+			if (escolha >= 1 && escolha <= 4 && aux == 1 && (temPP (head usuPkmns) escolha)) 
+				then do
+					putStrLn $ "Seu pokemon ataca primeiro" 
+					(novoAtacante, novoAlvo) <- realizaAtaque (head usuPkmns) (head enPkmns) escolha
+					let newUsuPkmns = [novoAtacante] ++ (tail usuPkmns)
+					let newEnPkmns = [novoAlvo] ++ (tail enPkmns)
+					if currentHp (head newEnPkmns) <= 0
+						then do batalha newUsuPkmns newEnPkmns rivais itens
+						else do
+							print "O pokemon do adversario ataca de volta"
+							(novoPkmnEn, novoPkmnUsu) <- realizaAtaque (head newEnPkmns) (head newUsuPkmns) melhorAtaque
+							let newUsuPkmns2 = [novoPkmnUsu] ++ (tail newUsuPkmns)
+							let newEnPkmns2 = [novoPkmnEn] ++ (tail newEnPkmns)
+							batalha newUsuPkmns2 newEnPkmns2 rivais itens
+			else if (escolha >= 1 && escolha <= 4 && aux == 2 && (temPP (head usuPkmns) escolha))
+				then do
+					print "O pokemon do adversario ataca primeiro"
+					(novoAtacante, novoAlvo) <- realizaAtaque (head enPkmns) (head usuPkmns) melhorAtaque
+					let newUsuPkmns = [novoAlvo] ++ (tail usuPkmns)
+					let newEnPkmns = [novoAtacante] ++ (tail enPkmns)
+					if currentHp (head newUsuPkmns) <= 0
+						then do batalha newUsuPkmns newEnPkmns rivais itens
+						else do
+							print "O seu pokemon ataca de volta"
+							(novoPkmnUsu, novoPkmnEn) <- realizaAtaque (head newUsuPkmns) (head newEnPkmns) escolha
+							let newUsuPkmns2 = [novoPkmnUsu] ++ (tail newUsuPkmns)
+							let newEnPkmns2 = [novoPkmnEn] ++ (tail newEnPkmns)
+							batalha newUsuPkmns2 newEnPkmns2 rivais itens
+			else do 
+				putStrLn $ "Entrada inválida"
+				batalha usuPkmns enPkmns rivais itens
 main::IO()
 main = do
-	pokemon1 <- coletaPokemon "Charizard"
-	pokemon2 <- coletaPokemon "Blastoise"
+	pokemon1 <- coletaPokemon "Blastoise"
+	pokemon2 <- coletaPokemon "Charizard"
+	pokemon3 <- coletaPokemon "Venusaur"
+	pokemon4 <- coletaPokemon "Aerodactyl"
 	let aux1 = extractMaybe $ extractEither pokemon1
 	let aux2 = extractMaybe $ extractEither pokemon2
+	let aux3 = extractMaybe $ extractEither pokemon3
+	let aux4 = extractMaybe $ extractEither pokemon4
 	pkmnBtl1 <- generatePokemon aux1
 	pkmnBtl2 <- generatePokemon aux2
-	(novoAtacante, novoAlvo) <- realizaAtaque pkmnBtl1 pkmnBtl2 1
-	(novoAtacante2, novoAlvo2) <- realizaAtaque novoAtacante novoAlvo 1
-	print novoAlvo2
---	print ataqueExecutado
---	print ataqueExecutado2
-
+	pkmnBtl3 <- generatePokemon aux3
+	pkmnBtl4 <- generatePokemon aux4
+	let item1 = Item {nomeItem = "Hyper Potion", qtde = 5}
+	let item2 = Item {nomeItem = "Full Restore", qtde = 5}
+	let nPkmnBtl1 = pkmnBtl1 {condition = "Envenenado"}
+	let list1 = [nPkmnBtl1, pkmnBtl2, pkmnBtl3, pkmnBtl4]
+	print "Aelson"
+	batalha [nPkmnBtl1, pkmnBtl2, pkmnBtl3, pkmnBtl4] [pkmnBtl2, pkmnBtl4] ["Aelson","Everton"] [item1, item2]
+--      mapM_ print $ trocaHeadTail [pkmnBtl1, pkmnBtl2, pkmnBtl3, pkmnBtl4]
